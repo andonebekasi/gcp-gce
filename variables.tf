f3bf0ef249785faa92e8135c1529ea67101286de
andi <34510976+andonebekasi@users.noreply.github.com>
2021-11-04 19:41:08 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "region" {asia-southeast2}
-variable "gcp_project" {poc-danamon-devsecops}
-variable "name" {dev}
-variable "subnet_cidr" {10.10.0.0/24}
+variable "region" = {asia-southeast2}
+variable "gcp_project" = {poc-danamon-devsecops}
+variable "name" = {dev}
+variable "subnet_cidr" = {10.10.0.0/24}
